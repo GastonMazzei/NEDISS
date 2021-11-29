969becb1d00ef5cd81cf35d655913fdbd6758527
gaston <gastonmazzei95@gmail.com>
2021-11-29 13:30:26 +0100

    [V1.0.0] update

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 # Commands to run NEDISS
 
 
-mpirun -oversubscribe  -x NRUNS=2 -x EQNUMBER=0 -x SOLVER=1 -x SOLVERDEPTH=2 -x K1=0.1666666666666666 -x K2=0.33333333333333333 -x K3=0.333333333333333 -x K4=0.16666666666666  -x TOPOLOGY=0 -x kneigh=3 -x proba=0.1234  -x NNODES=12 -x TEST=1 -x SEED=21234 -x OMP_THREAD_LIMIT=5  -x  OMP_NESTED=true -np 3  cmake-build-debug/cppprojct
+source simulation.conf.sh
+
+mpirun -oversubscribe  -x SAMPLING_FREQ=$SAMPLING_FREQ -x EQNUMBER=$EQNUMBER -x SOLVER=$SOLVER -x SOLVERDEPTH=$SOLVERDEPTH -x K1=$K1 -x K2=$K2 -x K3=$K3 -x K4=$K4  -x TOPOLOGY=$TOPOLOGY -x J=$J -x WMIN=$WMIN -x WMAX=$WMAX -x kneigh=$kneigh -x proba=$proba -x NRUNS=$NRUNS -x NNODES=$NNODES -x TEST=1 -x SEED=$SEED -x OMP_THREAD_LIMIT=$OMP_THREAD_LIMIT  -x  OMP_NESTED=true -np $NP  cmake-build-debug/cppprojct
+
+
 
 
